8f94c2a54f2d565e59bbb8477432f2c375f66fcf
Azamat Bekmuratov and Justin Roller <azamat@>
2018-05-08 16:16:45 -0700

    add basic front end.

---
+++ UniversityRegistrar/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using UniversityRegistrar.Models;
+using System;
+
+namespace UniversityRegistrar.Controllers
+{
+    public class HomeController : Controller
+    {
+      [HttpGet("/")]
+      public ActionResult Index()
+      {
+          return View();
+      }
+      
+
+    }
+}
