a182a7572ca2cf84fc02a74740aeca4ce2f9031c
Azamat Bekmuratov and Justin Roller <justin@>
2018-05-08 16:00:30 -0700

    add unit tests for model methods

--- UniversityRegistrar.Test/ModelTests/CourseTest.cs
+++ UniversityRegistrar.Test/ModelTests/CourseTest.cs
@@ -21,9 +21,56 @@ namespace UniversityRegistrar.Tests
           Course.DeleteAll();
           Student.DeleteAll();
         }
+        [TestMethod]
+        public void Save_SavesCourseToDatabase_CourseList()
+        {
+          Course testCourse = new Course("Physics", 11212);
+          testCourse.Save();
 
+          List<Course> testResult = Course.GetAllCourses();
+          List<Course> allCities = new List<Course> {testCourse};
 
+          CollectionAssert.AreEqual(testResult, allCities);
+        }
+        [TestMethod]
+        public void Save_DatabaseAssignsIdToObject_Id()
+        {
+           //Arrange
+           Course testCourse = new Course("Maths", 89892);
+           testCourse.Save();
 
+           //Act
+           Course savedCourse = Course.GetAllCourses()[0];
+
+           int result = savedCourse.GetId();
+           int testId = testCourse.GetId();
+
+           //Assert
+           Assert.AreEqual(testId, result);
+        }
+        [TestMethod]
+        public void Equals_OverrideTrueForSameName_Course()
+        {
+           //Arrange, Act
+           Course firstCourse = new Course("Maths", 89892);
+           Course secondCourse = new Course("Maths", 89892);
+
+           //Assert
+           Assert.AreEqual(firstCourse, secondCourse);
+        }
+        [TestMethod]
+        public void Find_FindsCourseInDatabase_Course()
+        {
+           //Arrange
+           Course testCourse = new Course("Data Structure", 71232);
+           testCourse.Save();
+
+           //Act
+           Course foundCourse = Course.Find(testCourse.GetId());
+
+           //Assert
+           Assert.AreEqual(testCourse, foundCourse);
+        }
 
     }
 }
--- UniversityRegistrar.Test/ModelTests/StudentTest.cs
+++ UniversityRegistrar.Test/ModelTests/StudentTest.cs
@@ -22,19 +22,19 @@ namespace UniversityRegistrar.Tests
           Course.DeleteAll();
         }
         [TestMethod]
-         public void Save_SavesStudentToDatabase_StudentList()
-         {
-           Student testStudent = new Student("John", "2018-08-22");
-           testStudent.Save();
+        public void Save_SavesStudentToDatabase_StudentList()
+        {
+          Student testStudent = new Student("John", "2018-08-22");
+          testStudent.Save();
 
-           List<Student> testResult = Student.GetAllStudents();
-           List<Student> allStudents = new List<Student> {testStudent};
+          List<Student> testResult = Student.GetAllStudents();
+          List<Student> allStudents = new List<Student> {testStudent};
 
-           CollectionAssert.AreEqual(testResult, allStudents);
-         }
-         [TestMethod]
-         public void Save_DatabaseAssignsIdToObject_Id()
-         {
+          CollectionAssert.AreEqual(testResult, allStudents);
+        }
+        [TestMethod]
+        public void Save_DatabaseAssignsIdToObject_Id()
+        {
            //Arrange
            Student testStudent = new Student("John", "2018-08-22");
            testStudent.Save();
@@ -47,6 +47,51 @@ namespace UniversityRegistrar.Tests
 
            //Assert
            Assert.AreEqual(testId, result);
-         }
+        }
+        [TestMethod]
+        public void Equals_OverrideTrueForSameName_Student()
+        {
+           //Arrange, Act
+           Student firstStudent = new Student("John", "2018-08-22");
+           Student secondStudent = new Student("John", "2018-08-22");
+
+           //Assert
+           Assert.AreEqual(firstStudent, secondStudent);
+        }
+        [TestMethod]
+        public void Find_FindsStudentInDatabase_Student()
+        {
+           //Arrange
+           Student testStudent = new Student("John", "2018-08-22");
+           testStudent.Save();
+
+           //Act
+           Student foundStudent = Student.Find(testStudent.GetId());
+
+           //Assert
+           Assert.AreEqual(testStudent, foundStudent);
+        }
+        [TestMethod]
+        public void GetCourses_ReturnsAllStudentCourses_CourseList()
+        {
+          //Arrange
+          Student testStudent = new Student("John", "2018-08-22");
+          testStudent.Save();
+
+          Course testCourse1 = new Course("Mathamatics", 8911);
+          testCourse1.Save();
+
+          Course testCourse2 = new Course("Chemistry", 7911);
+          testCourse2.Save();
+
+          //Act
+          testStudent.AddCourse(testCourse1);
+          testStudent.AddCourse(testCourse2);
+          List<Course> result = testStudent.GetCourses();
+          List<Course> testList = new List<Course> {testCourse1, testCourse2};
+
+          //Assert
+          CollectionAssert.AreEqual(testList, result);
+        }
     }
 }
